a3dc5688eb48b4ba014e5b48eb0a65c116f56dbd
abdulazizdevv <abdulazizkenjaoxunov@gmail.com>
2024-01-28 09:16:56 +0500

    https

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 // const token = localStorage.getItem("token");
-export const baseURL = `http://system.ajoyib-fastfood.uz`;
+export const baseURL = `https://system.ajoyib-fastfood.uz`;
 
 const axiosInstance = axios.create({
   baseURL: baseURL,
